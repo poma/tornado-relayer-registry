86ad7e4029827b4aa21823d6feae8ee699cf338a
h-ivor <ivor.vrdoljak@protonmail.ch>
2021-09-12 16:12:37 +0200

    directory structure, uni helper library, inital...

---
+++ contracts/libraries/UniswapV3OracleHelper.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+import { OracleLibrary } from "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
+import { IUniswapV3Factory } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+
+interface IERC20Decimals {
+  function decimals() external returns (uint8);
+}
+
+library UniswapV3OracleHelper {
+  IUniswapV3Factory public constant UniswapV3Factory = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
+
+  function getPriceOfTokenInToken(
+    address baseToken,
+    address quoteToken,
+    uint24 fee,
+    uint32 period
+  ) public returns (uint256) {
+    return
+      OracleLibrary.getQuoteAtTick(
+        OracleLibrary.consult(UniswapV3Factory.getPool(baseToken, quoteToken, fee), period),
+        uint128(10**IERC20Decimals(baseToken).decimals()),
+        baseToken,
+        quoteToken
+      );
+  }
+}
---
+++ hardhat.config.js
@@ -0,0 +1,78 @@
+require('dotenv').config()
+require('@nomiclabs/hardhat-ethers')
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-waffle')
+require('hardhat-spdx-license-identifier')
+require('hardhat-storage-layout')
+require('hardhat-log-remover')
+require('hardhat-contract-sizer')
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
+      {
+        version: '0.8.7',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.mainnet_rpc,
+        blockNumber: 13042331,
+      },
+      initialBaseFeePerGas: 5,
+      loggingEnabled: false,
+      allowUnlimitedContractSize: false,
+    },
+    localhost: {
+      url: 'http://localhost:8545',
+      timeout: 120000,
+    },
+    mainnet: {
+      url: process.env.mainnet_rpc,
+      accounts: [process.env.mainnet_account_pk],
+      timeout: 2147483647,
+    },
+    goerli: {
+      url: process.env.goerli_rpc,
+      accounts: [process.env.goerli_account_pk],
+      timeout: 2147483647,
+    },
+  },
+  mocha: { timeout: 9999999999 },
+  spdxLicenseIdentifier: {
+    overwrite: true,
+    runOnCompile: true,
+  },
+  etherscan: {
+    apiKey: process.env.etherscan_api_key,
+  },
+}
